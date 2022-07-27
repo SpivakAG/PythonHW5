56a1e99f20484e075a33c7a293af7e5a0da82615
ArtemSpivak <spivak25@yandex.ru>
2022-07-27 20:51:10 +0300

    обновлен gitignore, не решена задача 1

--- Задача1.py
+++ Задача1.py
@@ -1,1 +1,10 @@
 #Даны два файла, в каждом из которых находится запись многочлена. Задача - сформировать файл, содержащий сумму многочленов. Это не просто сумма всех коэффициентов
+from itertools import zip_longest
+text1=str(open('file1.txt', 'r').read()).split('+') #получаем списки с разделением по членам
+text2=str(open('file2.txt', 'r').read()).split('+')
+
+text1.reverse()
+text2.reverse()
+
+for i,j in zip_longest(text1, text2, fillvalue=0):
+    
